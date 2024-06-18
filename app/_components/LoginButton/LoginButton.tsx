c4d51deb04e363325232e253b166e9940505c4bd
sh-okada <sh-okada@ymail.ne.jp>
2024-06-19 07:36:57 +0900

    githubのログインページに直接遷移させる

--- app/_components/LoginButton/LoginButton.tsx
+++ app/_components/LoginButton/LoginButton.tsx
@@ -1,9 +1,10 @@
 "use client";
+
 import { EcButton } from "@/components";
 import { signIn } from "next-auth/react";
 
 const LoginButton = () => {
-  return <EcButton onClick={() => signIn()}>ログイン</EcButton>;
+  return <EcButton onClick={() => signIn("github")}>ログイン</EcButton>;
 };
 
 export { LoginButton };
