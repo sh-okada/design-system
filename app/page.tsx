7b96bc5b5923edfeb1cf69c1101f0c4e1c7c1048
sh-okada <sh-okada@ymail.ne.jp>
2024-06-24 00:04:15 +0900

    nextauthに関する記述削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import { LoginButton } from "./_components";
-import { EcTypography } from "@/components";
-
-const getREADME = async (username: string) => {
-  console.log(username);
-  const gitUrl = `https://api.github.com/repos/${username}/design-system/contents/README.md`;
-  const res = await fetch(gitUrl, {
-    headers: {
-      Accept: "application/vnd.github.v3+json",
-      Authorization: `token ${username}`,
-    },
-  });
-
-  const readme = await res.json();
-
-  console.log(readme);
-};
+import { EcTypography } from "@/components/core";
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-  console.log(user);
-  getREADME(user ? (user.accessToken as string) : "");
-
-  return user ? (
-    <EcTypography>{user.accessToken}</EcTypography>
-  ) : (
-    <LoginButton />
-  );
+  return <EcTypography>This is Top Page.</EcTypography>;
 }
