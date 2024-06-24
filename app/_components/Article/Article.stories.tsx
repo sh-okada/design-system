c145e74ab82fbb432c616f12286b276920c7a0b7
sh-okada <sh-okada@enecom.co.jp>
2024-06-24 13:54:09 +0900

    リファクタリング

--- app/_components/Article/Article.stories.tsx
+++ app/_components/Article/Article.stories.tsx
@@ -1,9 +1,6 @@
-import { fn } from "@storybook/test";
-
 import { Article } from "./Article";
 
 import type { Meta, StoryObj } from "@storybook/react";
-import { EcButton } from "@/components/core";
 
 const meta = {
   title: "Components/Article",
@@ -14,7 +11,7 @@ const meta = {
   tags: ["autodocs"],
   args: {
     href: "#",
-    label: "記事タイトル記事タイトル記事タイトル記事タイトル記事タイトル",
+    title: "記事タイトル記事タイトル記事タイトル記事タイトル記事タイトル",
     name: "sh-okada",
   },
 } satisfies Meta<typeof Article>;
